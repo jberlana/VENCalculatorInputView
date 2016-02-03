ada4c0fdf1702d3826fcb2015695d3b48d85698e
Javier Berlana <jberlana@gmail.com>
2016-02-03 13:50:35 +0100

    The user can not type more than one decimal separator per number.

--- VENCalculatorInputView/VENCalculatorInputTextField.m
+++ VENCalculatorInputView/VENCalculatorInputTextField.m
@@ -6,6 +6,11 @@
 @property (strong, nonatomic) VENMoneyCalculator *moneyCalculator;
 @end
 
+static NSString * const k_plus_operator     = @"+";
+static NSString * const k_minus_operator    = @"−";
+static NSString * const k_multiply_operator = @"×";
+static NSString * const k_divide_operator   = @"÷";
+
 @implementation VENCalculatorInputTextField
 
 - (id)initWithFrame:(CGRect)frame {
@@ -22,16 +27,16 @@
 
 - (void)setUpInit {
     self.locale = [NSLocale currentLocale];
-
+    
     VENCalculatorInputView *inputView = [VENCalculatorInputView new];
     inputView.delegate = self;
     inputView.locale = self.locale;
     self.inputView = inputView;
-
+    
     VENMoneyCalculator *moneyCalculator = [VENMoneyCalculator new];
     moneyCalculator.locale = self.locale;
     self.moneyCalculator = moneyCalculator;
-
+    
     [self addTarget:self action:@selector(venCalculatorTextFieldDidEndEditing) forControlEvents:UIControlEventEditingDidEnd];
 }
 
@@ -66,13 +71,10 @@
             return;
         }
     }
-
+    
     [self insertText:key];
     NSString *subString = [self.text substringToIndex:self.text.length - 1];
-    if ([key isEqualToString:@"+"] ||
-        [key isEqualToString:@"−"] ||
-        [key isEqualToString:@"×"] ||
-        [key isEqualToString:@"÷"]) {
+    if ([self stringContainsOperator:key]) {
         NSString *evaluatedString = [self.moneyCalculator evaluateExpression:[self trimExpressionString:subString]];
         if (evaluatedString) {
             self.text = [NSString stringWithFormat:@"%@%@", evaluatedString, key];
@@ -81,10 +83,13 @@
         }
     } else if ([key isEqualToString:[self decimalSeparator]]) {
         if (self.text.length == 1) {
-          self.text = [NSString stringWithFormat:@"0%@",[self decimalSeparator]];
+            self.text = [NSString stringWithFormat:@"0%@",[self decimalSeparator]];
         }
         NSString *secondToLastCharacterString = [self.text substringWithRange:NSMakeRange([self.text length] - 2, 1)];
         if ([secondToLastCharacterString isEqualToString:[self decimalSeparator]]) {
+            self.text = subString;
+        }
+        else if ([[self rigthSideOfExpression:subString] containsString:[self decimalSeparator]]) {
             self.text = subString;
         }
     }
@@ -94,8 +99,43 @@
     [self deleteBackward];
 }
 
-
 #pragma mark - Helpers
+
+/**
+ Returns the number at the right side of the operator.
+ @param expression The string to check.
+ @return The string at right side of the operator.
+ */
+- (NSString *)rigthSideOfExpression:(NSString *)expression {
+    NSString *rightSide = expression;
+    if ([self stringContainsOperator:expression]) {
+        if ([expression containsString:k_plus_operator]) {
+            rightSide = [expression substringFromIndex:[expression rangeOfString:k_plus_operator].location+1];
+        }
+        else if ([expression containsString:k_minus_operator]) {
+            rightSide = [expression substringFromIndex:[expression rangeOfString:k_minus_operator].location+1];
+        }
+        else if ([expression containsString:k_multiply_operator]) {
+            rightSide = [expression substringFromIndex:[expression rangeOfString:k_multiply_operator].location+1];
+        }
+        else if ([expression containsString:k_minus_operator]) {
+            rightSide = [expression substringFromIndex:[expression rangeOfString:k_minus_operator].location+1];
+        }
+    }
+    return rightSide;
+}
+
+/**
+ Checks if the given string contains any operator.
+ @param string The string to check.
+ @return YES if contains the operator.
+ */
+- (BOOL)stringContainsOperator:(NSString *)string {
+    return ([string containsString:k_plus_operator] ||
+            [string containsString:k_minus_operator] ||
+            [string containsString:k_multiply_operator] ||
+            [string containsString:k_divide_operator] );
+}
 
 /**
  Removes any trailing operations and decimals.
@@ -106,10 +146,7 @@
     NSString *txt = self.text;
     while ([txt length] > 0) {
         NSString *lastCharacterString = [txt substringFromIndex:[txt length] - 1];
-        if ([lastCharacterString isEqualToString:@"+"] ||
-            [lastCharacterString isEqualToString:@"−"] ||
-            [lastCharacterString isEqualToString:@"×"] ||
-            [lastCharacterString isEqualToString:@"÷"] ||
+        if ([self stringContainsOperator:lastCharacterString] ||
             [lastCharacterString isEqualToString:self.decimalSeparator]) {
             txt = [txt substringToIndex:txt.length - 1];
         }
